af2672cfd5609ac4a93d7c5eacfdf36e67626929
Quinault <aquinault@gmail.com>
2014-07-24 09:12:04 +0000

    MAJ

--- client/js/spinning.js
+++ client/js/spinning.js
@@ -13,7 +13,7 @@
     this._spin();
   }
 
-  Spinning.prototype._init = function() {
+  Spinning.prototype._init2 = function() {
     var spinnings = this.spinnings;
 
     $(this.icons).each(function(n) {
@@ -35,13 +35,60 @@
           function() {
             node.fadeTo(250, .6).css('zIndex', 1000);
             timer && clearTimeout(timer);
-            timer = setTimeout(spin, Math.ceil(random(10000)));
+            timer = setTimeout(spin, Math.ceil(random(5000)));
           }
       );
 
       function spin() {
         node.css('transform', 'rotate(' + startDeg + 'deg)');
       }
+
+      spinnings[n] = spin;
+    })
+
+    return this;
+  }
+
+  Spinning.prototype._init = function() {
+    var spinnings = this.spinnings;
+
+    $(this.icons).each(function(n) {
+      var startDeg = random(360);
+      var node = $(this);
+      var timer;
+
+      node.css({
+        top: random(40),
+        left: n * 50 + random(10),
+        zIndex: 1000
+      }).hover(
+          function() {
+            node.addClass('animated bounceOutLeft');
+
+
+            node.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+              node.removeClass('bounceOutLeft');
+              node.addClass('bounceInRight');
+            });
+
+/*            node.fadeTo(250, 1)
+                .css('zIndex', 1001)
+                .css('transform', 'rotate(0deg)');
+*/
+          },
+          function() {
+/*            node.fadeTo(250, .6).css('zIndex', 1000);
+            timer && clearTimeout(timer);
+            timer = setTimeout(spin, Math.ceil(random(5000)));
+*/
+          }
+      );
+
+
+      function spin() {
+        node.css('transform', 'rotate(' + startDeg + 'deg)');
+      }
+      
 
       spinnings[n] = spin;
     })
@@ -58,8 +105,45 @@
     return this;
   }
 
+  // -------------------------------------------
+/*  Spinning.prototype.chaos = function(node) {
+    var startDeg = 400;
+    node.css('transform', 'rotate(' + startDeg + 'deg)');
+  }
+*/
+
+  Spinning.prototype.home = function(node) {
+    node.fadeTo(250, 1).css('zIndex', 1001).css('transform', 'rotate(0deg)');
+    
+    var timer;
+    timer && clearTimeout(timer);
+    timer = setTimeout(chaos, 5000);
+    
+    
+    function chaos() {
+        var startDeg = 400;
+        node.css('transform', 'rotate(' + startDeg + 'deg)');
+    }
+
+  }
+
+
   function random(x) { return Math.random() * x };
 
-    // main
-    var s = new Spinning('#container');
-    s.render();
+  // main
+  var s = new Spinning('#container');
+  s.render();
+
+  window.addEventListener('load', function() {
+  	//var textInput = document.querySelector('input');
+  
+  	FastClick.attach(document.body);
+  	Array.prototype.forEach.call(document.getElementsByClassName('test'), function(testEl) {
+  		testEl.addEventListener('click', function() {
+  		  
+  		  s.home($(this));
+  		  //alert("click");
+  			//textInput.focus();
+  		}, false)
+  	});
+  }, false);
